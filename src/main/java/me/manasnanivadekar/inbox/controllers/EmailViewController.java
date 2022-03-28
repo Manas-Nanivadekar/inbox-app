b99f8109ddaaeb33dab78bd46c5fe81055eb0ea8
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2022-03-28 11:01:44 +0530

    email view and linking add

---
+++ src/main/java/me/manasnanivadekar/inbox/controllers/EmailViewController.java
@@ -0,0 +1,62 @@
+package me.manasnanivadekar.inbox.controllers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import me.manasnanivadekar.inbox.email.Email;
+import me.manasnanivadekar.inbox.email.EmailRepository;
+import me.manasnanivadekar.inbox.folders.Folder;
+import me.manasnanivadekar.inbox.folders.FolderRepository;
+import me.manasnanivadekar.inbox.folders.FolderService;
+
+@Controller
+public class EmailViewController {
+    // Folders
+    @Autowired
+    private FolderRepository folderRepository;
+    @Autowired
+    private FolderService folderService;
+
+    // Email
+    @Autowired
+    private EmailRepository emailRepository;
+
+    @GetMapping(value = "/emails/{id}")
+    public String emailView(
+            @PathVariable UUID id,
+            @AuthenticationPrincipal OAuth2User principal,
+            Model model) {
+
+        if (principal == null || !StringUtils.hasText(principal.getAttribute("login"))) {
+            return "index";
+        }
+
+        // Fetch Folders
+        String userId = principal.getAttribute("login");
+        List<Folder> userFolders = folderRepository.findAllById(userId);
+        model.addAttribute("userFolders", userFolders);
+        List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
+        model.addAttribute("defaultFolders", defaultFolders);
+
+        Optional<Email> optionalEmail = emailRepository.findById(id);
+        if (optionalEmail.isEmpty()) {
+            return "inbox-page";
+        }
+
+        Email email = optionalEmail.get();
+        String toIds = String.join(", ", email.getTo());
+        model.addAttribute("email", email);
+        model.addAttribute("toIds", toIds);
+        return "email-page";
+    }
+}
