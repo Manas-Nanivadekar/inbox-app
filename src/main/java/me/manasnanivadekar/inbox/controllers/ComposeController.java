93d22aff6a319d400d38c2efceb1f0a4f7fcc14b
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2022-03-28 14:29:49 +0530

    email send functionality add

--- src/main/java/me/manasnanivadekar/inbox/controllers/ComposeController.java
+++ src/main/java/me/manasnanivadekar/inbox/controllers/ComposeController.java
@@ -1,5 +1,6 @@
 package me.manasnanivadekar.inbox.controllers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,9 +10,15 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import me.manasnanivadekar.inbox.email.EmailService;
 import me.manasnanivadekar.inbox.folders.Folder;
 import me.manasnanivadekar.inbox.folders.FolderRepository;
 import me.manasnanivadekar.inbox.folders.FolderService;
@@ -24,6 +31,8 @@ public class ComposeController {
     private FolderRepository folderRepository;
     @Autowired
     private FolderService folderService;
+    @Autowired
+    private EmailService emailService;
 
     @GetMapping(value = "/compose")
     public String getComposePage(
@@ -41,16 +50,40 @@ public class ComposeController {
         List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
         model.addAttribute("defaultFolders", defaultFolders);
 
-        String[] splitIds = to.split(", ");
+        List<String> uniqueToIds = splitIds(to);
+        model.addAttribute("toIds", String.join(", ", uniqueToIds));
+
+        return "compose-page";
+    }
+
+    private List<String> splitIds(String to) {
+        if (!StringUtils.hasText(to)) {
+            return new ArrayList<String>();
+        }
+        String[] splitIds = to.split(",");
         List<String> uniqueToIds = Arrays.asList(splitIds)
                 .stream()
                 .map(id -> StringUtils.trimWhitespace(id))
                 .filter(id -> StringUtils.hasText(id))
                 .distinct()
                 .collect(Collectors.toList());
+        return uniqueToIds;
+    }
 
-        model.addAttribute("toIds", String.join(", ", uniqueToIds));
+    @PostMapping("/sendEmail")
+    public ModelAndView sendEmail(
+            @RequestBody MultiValueMap<String, String> formData,
+            @AuthenticationPrincipal OAuth2User principal) {
+        if (principal == null || !StringUtils.hasText(principal.getAttribute("login"))) {
+            return new ModelAndView("redirect:/");
+        }
 
-        return "compose-page";
+        String from = principal.getAttribute("login");
+        List<String> toIds = splitIds(formData.getFirst("toIds"));
+        String subject = formData.getFirst("subject");
+        String body = formData.getFirst("body");
+        emailService.sendEmail(from, toIds, subject, body);
+        return new ModelAndView("redirect:/");
+
     }
 }
